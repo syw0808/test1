6a9c9eb07e0f04a6c86041e43eac11dde8f0101a
root <root@centos6.8>
2020-07-20 16:35:01 +0900

    엽기 수 세서 파일에 write 하기

--- python016.py
+++ python016.py
@@ -1,10 +1,32 @@
 #! /usr/bin/env python
 import sys
 
+if len(sys.argv) != 2:
+    print(f"usage : python {sys.argv[0]} [fasta]")
+    sys.exit()
+
 f=sys.argv[1]
+dic={}
 
 with open(f,"r") as handle :
     for line in handle :
         if line.startswith(">"):
             continue
-        print(line.strip())
+        for s in line.strip():
+            if s in dic:
+                dic[s]+=1
+            else :
+                dic[s]=1
+       
+print(dic)
+total=0
+for k, v in dic.items():
+    total+=v
+print(total)
+
+with open("result.txt","w") as handle:
+    handle.write(f"A:{dic['A']}\n")
+    handle.write(f"C:{dic['C']}\n")
+    handle.write(f"G:{dic['G']}\n")
+    handle.write(f"T:{dic['T']}\n")
+
