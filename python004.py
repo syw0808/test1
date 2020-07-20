3f8153068cdfbe6e08ddeed42af8f8845ff8d382
root <root@centos6.8>
2020-07-20 14:16:01 +0900

    if-elif-else

---
+++ python004.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+num=3
+
+if num%2==0:
+    print("짝수")
+else : print("홀수")
---
+++ python005.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+n1=21
+if n1%3==0 :
+    if n1%7==0 :
+        print("3과 7의 배수")
+    else :
+        print("3의 배수")
+elif n%7==0:
+    print("7의 배수")
+   
